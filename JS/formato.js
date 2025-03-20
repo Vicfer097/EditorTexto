cd34e3c31d3fe2c8eee0025f7377097d08f37710
MakiMike <josemigueltalaverauribe@hotmail.com>
2025-03-20 09:51:17 -0600

    changes

---
+++ JS/formato.js
@@ -0,0 +1,36 @@
+function negritas() {
+    
+    document.execCommand('bold', false, null);
+}
+
+function cursiva() {
+    document.execCommand('italic', false, null);
+}
+
+function subrayado() {
+    document.execCommand('underline', false, null);
+}
+
+function tachado() {
+    document.execCommand('strikeThrough', false, null);
+}
+
+function alinearIzquierda() {
+    document.execCommand('justifyLeft', false, null);
+}
+
+function alinearCentro() {
+    document.execCommand('justifyCenter', false, null);
+}
+
+function alinearDerecha() {
+    document.execCommand('justifyRight', false, null);
+}
+
+function undo() {
+    document.execCommand('undo', false, null);
+}
+
+function redo() {
+    document.execCommand('redo', false, null);
+}
